3d3f7c0edb23ab367d50992558ce58ba18fdb99a
Gus Hahn-Powell <gushahnpowell@gmail.com>
2015-03-25 12:11:42 -0700

    Added filter to redups.

--- ja_redups.py
+++ ja_redups.py
@@ -23,7 +23,7 @@ class JapaneseReduplicator(object):
        self.vowel_positions = [i for i in range(self.length) if self.pattern[i] == "V"]
        self.consonant_positions = [i for i in range(self.length) if self.pattern[i] == "C"]
 
-    def make_reduplicants(self, letter, position):
+    def make_reduplicants(self):
         def valid_pattern(pattern):
            redup_candidate = "".join(pattern)
            if pattern[-1] != "n" and self.pattern[-1] == "C" or any(invalid in redup_candidate for invalid in self.invalid_sequences):
@@ -36,13 +36,15 @@ class JapaneseReduplicator(object):
            return True
 
         redups = sorted(set("".join(candidate) for candidate in product(self.sounds, repeat=self.length) if valid_pattern(candidate)))
-        return "\n".join(" ".join(d) for d in zip(redups, redups))
+        return [" ".join(d) for d in zip(redups, redups)]
 
-
+    def filter_reduplicants(self, letter, position):
+        return [redup for redup in self.make_reduplicants() if redup[position] == letter]
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print "USAGE:\n\tpython ja_redups.py <pattern> <letter> <index of letter (starts at zero)>\n\t\texample for 'pattern' => CVCV"
         sys.exit(0)
 
     reduplicator = JapaneseReduplicator(sys.argv[1])
-    print reduplicator.make_reduplicants(sys.argv[2], int(sys.argv[-1]))
+    filtered_reduplicants = reduplicator.filter_reduplicants(letter=sys.argv[2], position=int(sys.argv[-1]))
+    print "\n".join(filtered_reduplicants)
